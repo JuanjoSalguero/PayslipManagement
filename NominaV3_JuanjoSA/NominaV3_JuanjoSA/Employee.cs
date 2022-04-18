ad6090132a4432e80284797b73d08b45168772df
Juanjo Salguero <90873093+JuanjoSalguero@users.noreply.github.com>
2022-04-18 19:37:04 +0200

    Add files via upload
    C#

---
+++ NominaV3_JuanjoSA/NominaV3_JuanjoSA/Employee.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace NominaV3_JuanjoSA
+{
+    class Employee
+    {
+        // ----------------------------------------------------------------------- MIEMBROS
+        // CONSTANTES
+        private const byte MAX_NAME = 15;                    // Caracteres máximos para el nombre
+        private const byte MAX_SURNAMES = 30;                 // Caracteres máximos para los apellidos
+        private const byte MAX_CATEGORY = 15;                 // Caracteres máximos para la categoría
+
+        private const byte MAX_DNI = 9;                       // Caracteres del DNI
+
+        private const byte WEEKS = 5;                        // Semanas
+
+        // MIEMBROS
+        private string name;            // Nombre del trabajador
+        private string surnames;        // Apellidos del trabajador
+        private string dni;             // Dni del trabajador
+        private string category;        // Puesto de trabajo del trabajador
+
+        // ----------------------------------------------------------------------- NÓMINA MENSUAL
+
+        private List<Payslip> monthPay;
+
+        // ----------------------------------------------------------------------- CONSTRUCTOR
+
+        // Constructor obligatorio
+        public Employee(string dni) 
+        {
+            Name = "Unknow";
+            Surnames = "Unknow";
+            DniValidation(dni);
+            Category = "Unknow";
+            monthPay = new List<Payslip>();
+            //Inicialización Lista Payslip
+            for (int i = 0; i < WEEKS; i++) monthPay.Insert(i, new Payslip());
+        }
+
+        // ----------------------------------------------------------------------- PROPIEDADES DE LOS ATRIBUTOS
+
+        public string Name // Propiedad nombre 
+        {
+            get 
+            {
+                return name;
+            }
+            set 
+            {
+                Validation(value); // Validación string
+
+                // Validación caracteres máximos
+                if (value.Length > MAX_NAME) throw new Exception($"* El NOMBRE no puede contener más de {MAX_NAME} caracteres.");
+
+                name = SpacesReplace(value);
+            }
+        }
+ 
+        public string Surnames // Propiedad apellidos 
+        {
+            get 
+            {
+                return surnames;
+            }
+            set 
+            {
+                Validation(value); // Validación string
+
+                // Validación caracteres máximos
+                if (value.Length > MAX_SURNAMES) throw new Exception($"* Los APELLIDOS no puede contener más de {MAX_SURNAMES} caracteres.");
+
+                surnames = SpacesReplace(value);
+            }   
+        }
+
+        public string Dni => dni; // Propiedad dni (Sólo lectura)
+
+        public string Category // Propiedad puesto 
+        {
+            get 
+            {
+                return category;
+            }
+            set
+            {
+                Validation(value);
+
+                // Validación caracteres máximos
+                if (value.Length > MAX_CATEGORY) throw new Exception($"* La CATEGORIA no puede contener más de {MAX_CATEGORY} caracteres.");
+
+                category = SpacesReplace(value);
+            }
+        }
+
+        // ----------------------------------------------------------------------- PROPIEDAD DE LAS CONSTANTES
+
+        public byte Weeks => WEEKS; // Propiedad WEEKS
+
+        // ----------------------------------------------------------------------- MÉTODOS PRIVADOS - INTERNOS
+        
+        // Método para la validación de la cadena introducida
+        private void Validation(string value) 
+        {
+            value = value.ToUpper(); // Pasamos la cadena a UPPER para las validaciones
+
+            // Validación espacios en blanco
+            if (String.IsNullOrWhiteSpace(value)) throw new Exception("* Este campo no puede estar formado por espacios en blanco.");
+
+            // Validación campo vacío
+            if (String.IsNullOrEmpty(value)) throw new Exception("* Este campo no puede estar vacío.");
+
+            // Validación caracteres
+            for (int i = 0; i < value.Length; i++)
+            {
+                if (!((value[i] >= 65 && value[i] <= 90) || value[i] == 32 || value[i] == 'Ñ' ||
+                    value[i] == 'Á' || value[i] == 'É' || value[i] == 'Í' || value[i] == 'Ó' || value[i] == 'Ú'))
+                    throw new Exception("* La cadena introducida contiene caracteres no válidos.");
+            }
+        }
+
+        // Método para eliminar espacios duplicados en una cadena
+        private string SpacesReplace(string value)
+        {
+            // Suprimir los espacios en blanco sobrantes (cuando haya mas de uno seguido)
+            value = Regex.Replace(value, @"\s+", " "); // Elimina los espacios duplicados en 
+            // Elimina los espacios en blanco tanto del principio como del final
+            value = value.Trim();
+
+            return value;
+        }
+
+        // Método para la validación del DNI
+        private void DniValidation(string dni)
+        {
+            dni = dni.ToUpper(); // Pasamos a UPPER para la validación del ultimo caracter
+            // Validación de la longitud del Dni
+            if (dni.Length != MAX_DNI) throw new Exception("* El DNI debe contener 9 caracteres obligatoriamente.");
+
+            // Validación de los 8 números del dni (48 - 57)
+            for (int i = 0; i < MAX_DNI - 1; i++)
+            {
+                if (!(dni[i] >= 48 && dni[i] <= 57)) throw new Exception("* Los primero 8 caracteres del DNI deben ser números.");
+            }
+
+            // Validación de que el ultimo caracter del dni sea letra
+            if (!(dni[8] >= 65 && dni[8] <= 90)) throw new Exception("* El ultimo caracter del DNI debe ser una letra.");
+
+            // Validación de que sea un DNI correcto
+            // Extraemos los 8 primeros caracteres del string y hacemos el modulo de 23
+            char[] dniLetter = { 'T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E'};
+            if (dniLetter[Int32.Parse(dni.Substring(0, 8)) % 23] != dni[8])
+                throw new Exception("* El DNI introducido no es un DNI válido.");
+
+            this.dni = dni;
+        }
+
+        // ----------------------------------------------------------------------- MÉTODOS 
+
+        // Método para insertar/modificar las horas trabajadas
+        public void InsertWorkedHours(byte option, float hours)
+        {
+            monthPay[option - 1].WorkedHours = hours; // Horas
+        }
+
+        // Método para insertar el importe de las horas trabajadas
+        public void InsertHourlyWage(byte option, float wage)
+        {
+            monthPay[option - 1].HourlyWage = wage; // Importe
+        }
+
+        // Método de validación si la semana seleccionada ya ha sido creada (Insertar nómina)
+        public void CreatedWeekValidation(byte option)
+        {
+            // Validación de no modificar una semana sin estar introducida
+            if (monthPay[option - 1].WorkedHours != 0) throw new Exception("* Semana ya creada, para modificarla acceda a su correspondiente menú.");
+        }
+
+        // Método para devolver la Nómina al completo
+        public Payslip getMonthlyPayslip(int i)
+        {
+            return monthPay[i];
+        }
+
+        // Validación eliminación jornada y modificación jornada
+        public void WeekValidation(byte option, string message)
+        {
+            if (monthPay[option - 1].WorkedHours == 0)
+            {
+                throw new Exception(message);
+            }
+        }
+
+        // ----------------------------------------------------------------------- OVERRIDE 
+
+        // Método para consultar la nomina semanal
+        public string CheckWeek(byte i)
+        {
+            string week = " ";
+
+            week = " _____________________________________________________________________________________________________________________________________________________ \n" +
+                $"|  Puesto: {Category}\n\n|  DNI: {Dni}\t\tNombre: {Name}\t\tApellidos: {Surnames}\n" +
+                " _____________________________________________________________________________________________________________________________________________________ \n" +
+                $"|\t\t\tHoras\tEuros/Hora\tHoras Extra\tSal. Extra\tSal. Base\tSal. Bruto\tRetención({monthPay[0].Irpf}%)        Sal. Neto\n" +
+                " _____________________________________________________________________________________________________________________________________________________ \n" +
+                $"|\tSemana {i}" +
+                $"\t{monthPay[i - 1].WorkedHours}\t{monthPay[i - 1].HourlyWage}\t\t{monthPay[i - 1].getOvertimeHours}" +
+                $"\t\t{monthPay[i - 1].getOvertimeSalary}\t\t{monthPay[i - 1].getBasicSalary}\t\t{monthPay[i - 1].getGrossSalary}" +
+                $"\t\t{monthPay[i - 1].getIncomeTax}\t                {monthPay[i - 1].getNetSalary}\n" +
+                " _____________________________________________________________________________________________________________________________________________________ \n" +
+                " _____________________________________________________________________________________________________________________________________________________ \n";
+
+            return week;
+        }
+
+        // Método para imprimir la nómina 
+        public override string ToString()
+        {
+            string print = "";
+
+            print = " _____________________________________________________________________________________________________________________________________________________ \n" +
+                $"|  Puesto: {Category}\n\n|  DNI: {Dni}\t\tNombre: {Name}\t\tApellidos: {Surnames}\n" +
+                " _____________________________________________________________________________________________________________________________________________________ \n" +
+                $"|\t\t\tHoras\tEuros/Hora\tHoras Extra\tSal. Extra\tSal. Base\tSal. Bruto\tRetención({monthPay[0].Irpf}%)        Sal. Neto\n" +
+                " _____________________________________________________________________________________________________________________________________________________ \n";
+            for (int i = 0; i < Weeks; i++)
+            {
+                print += $"|\tSemana {i + 1}" +
+                    $"\t{monthPay[i].WorkedHours}\t{monthPay[i].HourlyWage}\t\t{monthPay[i].getOvertimeHours}" +
+                    $"\t\t{monthPay[i].getOvertimeSalary}\t\t{monthPay[i].getBasicSalary}\t\t{monthPay[i].getGrossSalary}" +
+                    $"\t\t{monthPay[i].getIncomeTax}\t                {monthPay[i].getNetSalary}\n" +
+                    " _____________________________________________________________________________________________________________________________________________________ \n";
+            }
+            print += " _____________________________________________________________________________________________________________________________________________________ \n" +
+                "|  TOTAL MES:" +
+                $"\t\t{monthPay.Sum(x => x.WorkedHours)}\t\t\t{monthPay.Sum(x => x.getOvertimeHours)}\t\t" +
+                $"{Math.Round(monthPay.Sum(x => x.getOvertimeSalary), 2)}\t\t{Math.Round(monthPay.Sum(x => x.getBasicSalary), 2)}\t\t{Math.Round(monthPay.Sum(x => x.getGrossSalary), 2)}" +
+                $"\t\t{Math.Round(monthPay.Sum(x => x.getIncomeTax), 2)}\t                {Math.Round(monthPay.Sum(x => x.getNetSalary), 2)}\n" +
+                " _____________________________________________________________________________________________________________________________________________________ \n";
+
+            return print;
+        }
+    }
+}
---
+++ NominaV3_JuanjoSA/NominaV3_JuanjoSA/EmployeeM/E_Interface.cs
@@ -0,0 +1,257 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NominaV3_JuanjoSA
+{
+    static class E_Interface
+    {
+        // ----------------------------------------------------------------------- MÉTODOS DE SOLICITUD DE DATOS
+
+        // Método de solicitud de la información personal del trabajador            (Employee)
+        static public Employee EmployeeInformation(Employee employee)
+        {
+            bool check = false;
+
+            if (employee == null) // Si los datos del empleado están vacíos
+            {
+                EmployeeHeader(); // Cabecera
+
+                // Inicialización constructor, solicitud y lectura del dato obligatorio DNI
+                do
+                {
+                    try
+                    {
+                        employee = new Employee(ReadString("\tIntroduzca el DNI del trabajador: "));
+                        check = true;
+                    }
+                    catch (Exception e)
+                    {
+                        Principal_Interface.ErrorCatcher(e.Message);
+                    }
+                } while (!check);
+
+                // Solicitud y Lectura nombre
+                employee = Name(employee);
+
+                // Solicitud y Lectura apellidos
+                employee = Surnames(employee);
+
+                // Solicitud y Lectura apellidos
+                employee = Category(employee);
+
+                // Mensaje de confirmación
+                Console.Clear();
+                Console.BackgroundColor = ConsoleColor.Green;
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.WriteLine($"\n\tEl trabajador {employee.Surnames}, {employee.Name} con DNI {employee.Dni} como {employee.Category}");
+                Console.WriteLine($"\tha sido creado correctamente.                                            ");
+                Console.WriteLine("\tPresione ENTER para continuar...                                         ");
+                Console.ResetColor();
+                Console.ReadLine();
+            }
+            else // Si ya se han introducido datos del trabajador
+            {
+                Console.BackgroundColor = ConsoleColor.DarkRed;
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.WriteLine($"\n\tYa han sido insertados datos de un TRABAJADOR, si desea modificarlos, vuelva al menu anterior.");
+                Console.WriteLine("\tPresione ENTER para continuar...                                                              ");
+                Console.ResetColor();
+                Console.ReadLine();
+            }
+
+            return employee;
+        }
+
+        // Metodos de solicitud de Nombre, apellidos y categoría
+        static public Employee Name(Employee employee)
+        {
+            bool check = false;
+
+            do
+            {
+                try
+                {
+                    employee.Name = ReadString("\tIntroduzca el NOMBRE del trabajador: ");
+                    check = true;
+                }
+                catch (Exception e)
+                {
+                    Principal_Interface.ErrorCatcher(e.Message);
+                }
+            } while (!check);
+
+            return employee;
+        }
+        static public Employee Surnames(Employee employee)
+        {
+            bool check = false;
+
+            do
+            {
+                try
+                {
+                    employee.Surnames = ReadString($"\tIntroduzca los APELLIDOS de {employee.Name}: ");
+                    check = true;
+                }
+                catch (Exception e)
+                {
+                    Principal_Interface.ErrorCatcher(e.Message);
+                }
+            } while (!check);
+
+            return employee;
+        }
+        static public Employee Category(Employee employee)
+        {
+            bool check = false;
+
+            do
+            {
+                try
+                {
+                    employee.Category = ReadString($"\tIntroduzca el PUESTO de {employee.Surnames}, {employee.Name}: ");
+                    check = true;
+                }
+                catch (Exception e)
+                {
+                    Principal_Interface.ErrorCatcher(e.Message);
+                }
+            } while (!check);
+
+            return employee;
+        }
+
+        // ----------------------------------------------------------------------- MÉTODOS DE MODIFICACIÓN DE DATOS
+
+        // Menú de modificación información del empleado
+        static public void ModifyEmployee(Employee employee)
+        {
+            //CONSTANTES
+            // VARIABLES
+            byte option = 1; // Variable para las opciones del menú
+            ConsoleKey key = 0; // Inicialización método ConsoleKey con el nombre de variable key
+            bool exit = false; // Booleana para salir del bucle
+
+            // ENTRADA
+            if (employee == null)
+            {
+                Console.BackgroundColor = ConsoleColor.DarkRed;
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.WriteLine($"\n\tPara modificar los datos de un trabajador, antes debe estar creado el trabajador.");
+                Console.WriteLine("\tPresione ENTER para continuar...                                                 ");
+                Console.ResetColor();
+                Console.ReadLine();
+            }
+            else
+            {
+                do
+                {
+                    do
+                    {
+                        EmployeeModifyHeader(); // Cabecera
+
+                        // OPCIÓN 1
+                        if (option == 1) Principal_Interface.Option($"\t\t\t\t 01.    Nombre: \t{employee.Name} ");
+                        else Console.WriteLine($"\t\t\t\t 01.    Nombre: \t{employee.Name}");
+
+                        // OPCIÓN 2
+                        if (option == 2) Principal_Interface.Option($"\t\t\t\t 02.    Apellidos: \t{employee.Surnames} ");
+                        else Console.WriteLine($"\t\t\t\t 02.    Apellidos: \t{employee.Surnames} ");
+
+                        // OPCIÓN 3
+                        if (option == 3) Principal_Interface.Option($"\t\t\t\t 03.    Puesto: \t{employee.Category} ");
+                        else Console.WriteLine($"\t\t\t\t 03.    Puesto: \t{employee.Category} ");
+
+                        // OPCIÓN 4
+                        if (option == 4) Principal_Interface.Option("\t\t\t\t 04.    Salir");
+                        else Console.WriteLine("\t\t\t\t 04.    Salir");
+
+                        key = Console.ReadKey(true).Key; // Lectura de la tecla
+
+                        // Si presionamos la flecha Arriba
+                        if (key == ConsoleKey.UpArrow)
+                        {
+                            if (option == 1) option = 4;
+                            else option--;
+                        }
+
+                        // Si presionamos la fleca Abajo
+                        if (key == ConsoleKey.DownArrow)
+                        {
+                            if (option == 4) option = 1;
+                            else option++;
+                        }
+                        Console.Clear();
+
+                    } while (key != ConsoleKey.Enter); // Repetir el bucle mientras no se presione ENTER
+
+                    // PROCESO
+
+                    switch (option)
+                    {
+                        case 1:
+                            Name(employee);
+                            break;
+                        case 2:
+                            Surnames(employee);
+                            break;
+                        case 3:
+                            Category(employee);
+                            break;
+                        case 4:
+                            exit = true;
+                            break;
+                    }
+                    Console.Clear();
+
+                } while (!exit);
+            }
+            Console.Clear();
+        }
+
+        // ----------------------------------------------------------------------- LECTURA DE DATOS
+
+        // Método para la lectura de String
+        static public string ReadString(string message)
+        {
+            // CONSTANTES
+            // VARIABLES
+            string aux = "";
+
+            // ENTRADA
+            Console.BackgroundColor = ConsoleColor.DarkYellow;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine("\n\t" + message + "\n");
+            Console.ResetColor();
+            Console.Write("\t\t");
+            aux = Console.ReadLine();
+
+            // PROCESO
+            // SALIDA
+            return aux;
+        }
+
+        // ----------------------------------------------------------------------- HEADERS
+
+        // Cabecera del empleado
+        public static void EmployeeHeader()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("\t ------------------------------------------------------------------------------");
+            Console.WriteLine("\t|                    Introducción de datos del trabajador                      |");
+            Console.WriteLine("\t ------------------------------------------------------------------------------\n");
+            Console.ResetColor();
+        }
+
+        // Cabecera de la modificación de los datos del trabajador
+        public static void EmployeeModifyHeader()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("\t ------------------------------------------------------------------------------");
+            Console.WriteLine("\t|         Modificación datos del trabajador ¿Cuál desea modificar?             |");
+            Console.WriteLine("\t ------------------------------------------------------------------------------\n");
+            Console.ResetColor();
+        }
+    }
+}
